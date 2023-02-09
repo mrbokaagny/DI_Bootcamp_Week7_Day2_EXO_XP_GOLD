0f38ff9b99046e8bec3f1dc2ac5ec46017ff1138
mrdevbanks <103212600+mrdevbanks@users.noreply.github.com>
2023-02-09 08:01:57 +0000

    projet fini

---
+++ src/exercice2/Number.java
@@ -0,0 +1,29 @@
+package exercice2;
+
+/*
+*
+*@author Mr Boka
+*
+*/
+
+public class Number {
+    private int nombre;
+
+    public Number(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+}
+
+/*
+*
+*@author Mr Boka
+*
+*/
